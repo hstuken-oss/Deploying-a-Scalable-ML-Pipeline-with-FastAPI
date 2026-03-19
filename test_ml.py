1569f063189835addf9307a18073743e23dd1aef
hstuken_oss <hstuken@wgu.edu>
2026-03-19 14:34:35 +0000

    Add unit tests for train_model, compute_model_meterics, and inference

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,46 @@
 import pytest
-# TODO: add necessary import
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from ml.model import train_model, compute_model_metrics, inference
+
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_train_model():
     """
-    # add description for the first test
+    Test that the train_model returns a Random Forest Classifier that 
+    has been fitted on the training data.
     """
-    # Your code here
-    pass
+    X_train = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    y_train = np.array([0, 1, 0, 1])
+    model = train_model(X_train, y_train)
+    assert isinstance(model, RandomForestClassifier)
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_compute_model_metrics():
     """
-    # add description for the second test
+    Test that the compute_model_metrics returns precision, recall, and fbeta
+     as floats with values betweeen 0 and 1.
     """
-    # Your code here
-    pass
+    y = np.array([1, 0, 1, 0, 1])
+    preds = np.array([1, 0, 1, 1, 0])
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+    assert 0 <= precision <= 1
+    assert 0 <= recall <= 1
+    assert 0 <= fbeta <= 1
+   
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_inference():
     """
-    # add description for the third test
+   Test that inference returns predictions as a numpy array 
+   with the same number of rows as the input data.
     """
-    # Your code here
-    pass
+    X_train = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    y_train = np.array([0, 1, 0, 1])
+    model = train_model(X_train, y_train)
+    X_test = np.array([[1, 2], [3, 4]])
+    preds = inference(model, X_test)
+    assert len(preds) == len(X_test)
